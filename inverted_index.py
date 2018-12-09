b200810bb5cce17dae8302def2b6b3a165b764da
Krishna Kumar Dey <krishnakumar.d16@iiits.in>
2018-12-09 20:49:52 +0530

    added soundex and fixed inverted index

--- inverted_index.py
+++ inverted_index.py
@@ -6,73 +6,12 @@ import phonetics
 # from nltk.stem import PorterStemmer
 # from nltk.stem import LancasterStemmer
 from tokenization import *
-
+from soundex import soundex
 
 #Directory of this file
 BASE_DIR  = os.path.dirname(os.path.realpath(__file__))
 # Location of the files to be indexed
 FILE_ROOT = os.path.join(BASE_DIR, 'inputFiles')
-
-
-def soundex(query: str):
-
-    # Step 0: Clean up the query string
-    query = query.lower()
-    letters = [char for char in query if char.isalpha()]
-
-    # Step 1: Save the first letter. Remove all occurrences of a, e, i, o, u, y, h, w.
-
-    # If query contains only 1 letter, return query+"000" (Refer step 5)
-    if len(query) == 1:
-        return query + "000"
-
-    to_remove = ('a', 'e', 'i', 'o', 'u', 'y', 'h', 'w')
-
-    first_letter = letters[0]
-    letters = letters[1:]
-    letters = [char for char in letters if char not in to_remove]
-
-    if len(letters) == 0:
-        return first_letter + "000"
-
-    # Step 2: Replace all consonants (include the first letter) with digits according to rules
-
-    to_replace = {('b', 'f', 'p', 'v'): 1, ('c', 'g', 'j', 'k', 'q', 's', 'x', 'z'): 2,
-                  ('d', 't'): 3, ('l',): 4, ('m', 'n'): 5, ('r',): 6}
-
-    first_letter = [value if first_letter else first_letter for group, value in to_replace.items()
-                    if first_letter in group]
-    letters = [value if char else char
-               for char in letters
-               for group, value in to_replace.items()
-               if char in group]
-
-    # Step 3: Replace all adjacent same digits with one digit.
-    letters = [char for ind, char in enumerate(letters)
-               if (ind == len(letters) - 1 or (ind+1 < len(letters) and char != letters[ind+1]))]
-
-    # Step 4: If the saved letter’s digit is the same the resulting first digit, remove the digit (keep the letter)
-    if first_letter == letters[0]:
-        letters[0] = query[0]
-    else:
-        letters.insert(0, query[0])
-
-    # Step 5: Append 3 zeros if result contains less than 3 digits.
-    # Remove all except first letter and 3 digits after it.
-
-    first_letter = letters[0]
-    letters = letters[1:]
-
-    letters = [char for char in letters if isinstance(char, int)][0:3]
-
-    while len(letters) < 3:
-        letters.append(0)
-
-    letters.insert(0, first_letter)
-
-    string = "".join([str(l) for l in letters])
-
-    return string
 
 
 
@@ -114,49 +53,52 @@ def inverted_index_add(inverted, doc_id, doc_index):
 
 
 if __name__ == '__main__':
+    phonetic_dict = {'1':'soundex','2':'dmetaphone'}
 
-    print("Type 'soundex' to create soundex_inverted_index:")
+    print("Type '1' to create soundex_inverted_index:")
 
-    print("Type 'dmetaphone' to create double_metaphone_inverted_index:")
+    print("Type '2' to create double_metaphone_inverted_index:")
 
-    phonetic_type=input()
+    phonetic_type=phonetic_dict.get(input(),None)
 
     if phonetic_type== 'soundex' or phonetic_type == 'dmetaphone':
-        pass
+        inverted = {}
+        doc_ids_names={}
+        
+        doc_id = 1
+        # Build Inverted-Index for documents
+        for filename in os.listdir(FILE_ROOT):
+            if filename.endswith(".txt"):
+                doc_ids_names[doc_id]=filename
+                file_location = os.path.join(FILE_ROOT, filename)
+                doc_file = open(file_location,"r")
+                print(filename)
+                text = doc_file.read()
+                doc_index = inverted_index(text)
+                inverted_index_add(inverted, doc_id, doc_index)
+                doc_id += 1
+
+        #open file to store the index
+        if phonetic_type == 'soundex':
+            file = open('soundex_index', 'wb')
+        else:
+            file = open('dmetaphone_index', 'wb')
+
+        # dump information to that file
+        pickle.dump(inverted, file)
+
+        # close the file
+        file.close()
+
+        file1 = open('Documents_info','wb')
+        pickle.dump(doc_ids_names, file1)
+        file1.close()
+        print("Files Indexed.")
     else:
         print("You entered wrong input")
 
-    inverted = {}
-    doc_ids_names={}
+
     
-    doc_id = 1
-    # Build Inverted-Index for documents
-    for filename in os.listdir(FILE_ROOT):
-        if filename.endswith(".txt") and doc_id <=2:
-            doc_ids_names[doc_id]=filename
-            file_location = os.path.join(FILE_ROOT, filename)
-            doc_file = open(file_location,"r")
-            print(filename)
-            text = doc_file.read()
-            doc_index = inverted_index(text)
-            inverted_index_add(inverted, doc_id, doc_index)
-            doc_id += 1
-
-    #open file to store the index
-    if phonetic_type == 'soundex':
-        file = open('soundex_index', 'wb')
-    else:
-        file = open('dmetaphone_index', 'wb')
-
-    # dump information to that file
-    pickle.dump(inverted, file)
-
-    # close the file
-    file.close()
-
-    file1 = open('Documents_info','wb')
-    pickle.dump(doc_ids_names, file1)
-    file1.close()
 
 
     # for word, doc_locations in inverted.items():
--- project.py
+++ project.py
@@ -4,73 +4,10 @@ import pickle
 from collections import OrderedDict
 from tokenization import * 
 from difflib import SequenceMatcher
-
+import sys
+from soundex import soundex
 
 retrieved_docs_list=[]
-
-def soundex(query: str):
-   
-    # Step 0: Clean up the query string
-    query = query.lower()
-    letters = [char for char in query if char.isalpha()]
-
-    # Step 1: Save the first letter. Remove all occurrences of a, e, i, o, u, y, h, w.
-
-    # If query contains only 1 letter, return query+"000" (Refer step 5)
-    if len(query) == 1:
-        return query + "000"
-
-    to_remove = ('a', 'e', 'i', 'o', 'u', 'y', 'h', 'w')
-
-    first_letter = letters[0]
-    letters = letters[1:]
-    letters = [char for char in letters if char not in to_remove]
-
-    if len(letters) == 0:
-        return first_letter + "000"
-
-    # Step 2: Replace all consonants (include the first letter) with digits according to rules
-
-    to_replace = {('b', 'f', 'p', 'v'): 1, ('c', 'g', 'j', 'k', 'q', 's', 'x', 'z'): 2,
-                  ('d', 't'): 3, ('l',): 4, ('m', 'n'): 5, ('r',): 6}
-
-    first_letter = [value if first_letter else first_letter for group, value in to_replace.items()
-                    if first_letter in group]
-    letters = [value if char else char
-               for char in letters
-               for group, value in to_replace.items()
-               if char in group]
-
-    # Step 3: Replace all adjacent same digits with one digit.
-    letters = [char for ind, char in enumerate(letters)
-               if (ind == len(letters) - 1 or (ind+1 < len(letters) and char != letters[ind+1]))]
-
-    # Step 4: If the saved letter’s digit is the same the resulting first digit, remove the digit (keep the letter)
-    if first_letter == letters[0]:
-        letters[0] = query[0]
-    else:
-        letters.insert(0, query[0])
-
-    # Step 5: Append 3 zeros if result contains less than 3 digits.
-    # Remove all except first letter and 3 digits after it.
-
-    first_letter = letters[0]
-    letters = letters[1:]
-
-    letters = [char for char in letters if isinstance(char, int)][0:3]
-
-    while len(letters) < 3:
-        letters.append(0)
-
-    letters.insert(0, first_letter)
-
-    string = "".join([str(l) for l in letters])
-
-    return string
-
-
-
-
 
 def get_levenshtein_distance(word1, word2):
     word2 = word2.lower()
@@ -152,12 +89,12 @@ def check_similarity(word):
     return similarity_dict
 
 
+phonetic_dict = {'1':'soundex','2':'dmetaphone'}
+print("Type '1' to use soundex_inverted_index:")
 
-print("Type 'soundex' to use soundex_inverted_index:")
+print("Type '2' to use double_metaphone_inverted_index:")
 
-print("Type 'dmetaphone' to use double_metaphone_inverted_index:")
-
-phonetic_type=input()
+phonetic_type=phonetic_dict.get(input(),None)
 # open a file, where inverted index is stored
 if phonetic_type =="soundex":
     file = open('soundex_index', 'rb')
@@ -165,6 +102,7 @@ elif phonetic_type =="dmetaphone":
     file = open('dmetaphone_index', 'rb')
 else:
     print("You entered wrong input")
+    sys.exit()
 
 # get the informationh from file
 inverted_index = pickle.load(file)
@@ -175,8 +113,7 @@ file.close()
 final=OrderedDict()
 query=input('Enter query:\n')
 changed_query = [word for _, word in word_index(query)]
-#print(changed_query)
-query=query.split()
+query=[x.lower() for x in query.split()]
 positions=[]
 flag=0
 for word in query:
@@ -186,7 +123,7 @@ for word in query:
     if word not in changed_query:
         final[name]=[(word,-1)]
     else:
-        if word in inverted_index:  
+        if word in inverted_index:
             final[name]=[(word,-1)]
             retrieve_docs(word)
         else:
@@ -213,10 +150,9 @@ if flag==1:
     print ("Your suggested query from our limited vocabulary is:") 
     print (form_final_query(final))
 
-#print (retrieved_docs_list)
 
 final_doc_set = set.union(*retrieved_docs_list)
-print(final_doc_set)
+# print(final_doc_set)
 
 # for word, doc_locations in inverted.items():
 #     print (word, doc_locations)
@@ -227,7 +163,7 @@ file1.close()
 
 for doc_id in final_doc_set:
     doc_name=doc_ids_names[doc_id]
-    print(doc_id,'.',doc_name)
+    print(str(doc_id)+'.',doc_name)
 
 
 
